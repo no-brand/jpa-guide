00d05db9b4b1e97a03619f73d3c6214c51973ecf
no-brand <do.dream.david@gmail.com>
2022-06-06 00:11:04 +0900

    style(standalone): change to java style

--- jpa-standalone/src/main/java/com/nobrand/jpa/standalone/Main.java
+++ jpa-standalone/src/main/java/com/nobrand/jpa/standalone/Main.java
@@ -1,7 +1,7 @@
 package com.nobrand.jpa.standalone;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("jpa-standalone:main");
     }
 }
