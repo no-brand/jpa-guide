c318daca06eb28b49757185f3d1a745880c00d7b
no-brand <do.dream.david@gmail.com>
2022-06-06 15:51:26 +0900

    feat(standalone): basic CRUD operations

--- jpa-standalone/h2/init-h2.sh
+++ jpa-standalone/h2/init-h2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#----------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------#
 # Driver Class | org.h2.Driver
-# JDBC URL     | jdbc:h2:~/jpa-guide/jpa-standalone/h2/database
+# JDBC URL     | jdbc:tcp:h2://localhost/~/dev/repo/jpa/jpa-guide/jpa-standalone/h2/database
 # Username     | admin
 # Password     | admin
-#----------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------#
 wget https://github.com/h2database/h2database/releases/download/version-2.1.212/h2-2022-04-09.zip ./h2-2022-04-09.zip
 unzip h2-2022-04-09.zip
 nohup sh ./h2/bin/h2.sh > h2.out &
